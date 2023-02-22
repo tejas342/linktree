c5bcad9ccc65f9d4c17fd3122afcdd895f509aaf
Tejas Prajapati <86593289+tejas342@users.noreply.github.com>
2023-02-22 14:38:17 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,48 +15,6 @@ export default function Home() {
           name="title"
           content="7 RAJ AIR SQN | Linktree | Air Squadron of Kota Group"
         />
-        <meta
-          name="description"
-          content=" Hi, I am Tejas Prajapati gfigefieifge"
-        />
-        {/* <meta
-          name="keywords"
-          content="Full-Stack Web Developer, Web Designer, MERN Stack, LAMP Stack, Portfolio, Anirudh, Soni"
-        /> */}
-        <meta name="robots" content="index, follow" />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="language" content="English" />
-        <meta name="revisit-after" content="0 days" />
-        {/* <meta name="author" content="Anirudh Soni" /> */}
-
-        {/* <!-- Open Graph / Facebook --> */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://linktree.anirudhsoni.in/" />
-        <meta
-          property="og:title"
-          content="Anirudh Soni | Linktree | Web Developer"
-        />
-        <meta
-          property="og:description"
-          content=" Hi, I am Anirudh Soni a web developer from Rajasthan who focuses on user interfaces, including rich experiences, design."
-        />
-        <meta property="og:image" content="/assets/images/banner.png" />
-
-        {/* <!-- Twitter --/> */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:url"
-          content="https://linktree.anirudhsoni.in/"
-        />
-        <meta
-          property="twitter:title"
-          content="Anirudh Soni | Linktree | Web Developer"
-        />
-        <meta
-          property="twitter:description"
-          content=" Hi, I am Anirudh Soni a web developer from Rajasthan who focuses on user interfaces, including rich experiences, design."
-        />
-        <meta property="twitter:image" content="/assets/images/banner.png" />
       </Head>
       <Background />
       <Header />
@@ -64,7 +22,7 @@ export default function Home() {
       {/* <Spotify /> */}
       <Links />
       <footer className="py-8 text-lg">
-        &lt;/&gt; by{" "}
+        &lt;/&gt; by {" "}
         <a
           href="https://www.instagram.com/_te_jas_/"
           className="text-blue-600 font-bold"
